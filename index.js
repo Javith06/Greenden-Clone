aded099dddc1ced5f22088d733e10da2a37b50f5
Mohammed Javith <miistahjavith@gmail.com>
2025-04-28 18:16:34 +0530

    Update index.js
    Samll changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 
    var sidenav = document.getElementById("sidenav")
-   var menuicon=document.getElementById("menuicon")
+   var menuicon=document.getElementById("menuicon") 
    var closenav=document.getElementById("closenav")
 
    menuicon.addEventListener("click",function(){
